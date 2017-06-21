c0b8f4ed01eaa3df97e525e40b702f6796917f98
Ismail Hassani <ismail.hassani@i-synergy.nl>
2017-06-21 19:11:10 +0200

    Added shared project for classes and interfaces

--- src/Mollie.Net/Properties/AssemblyInfo.cs
+++ src/Mollie.Net/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Mollie.Net")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("International Intelligence")]
 [assembly: AssemblyProduct("Mollie.Net")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
