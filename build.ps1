553a0dc415adf0ac1f08068d6aaf7182a0edd356
Ismail Hassani <ismail.hassani@i-synergy.nl>
2019-08-13 17:32:18 +0200

    Several improvements
    - added build script
    - updated nuget references
    -- Microsoft.NET.Test.Sdk
    -- Xunit
    -- Xunit.Runner.VisualStudio
    -- Newtonsoft.Json
    - removed obsolete nuget.exe file

---
+++ build.ps1
@@ -0,0 +1,23 @@
+$framework_version = "2.0.3.0"
+$nuget_version = "2.0.3"
+$msbuild = "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe"
+$sourceNugetExe = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+$targetNugetExe = "nuget.exe"
+$ErrorActionPreference = "Stop"
+
+Write-Host "Download latest nuget"
+Invoke-WebRequest $sourceNugetExe -OutFile $targetNugetExe
+Set-Alias nuget $targetNugetExe -Scope Global -Verbose
+
+Write-Host "First build all projects in Release mode (Platform should be both x86 and x64)"
+
+Write-Host "Build for .Net Standard 1.1"
+dotnet restore src\Mollie.NetStandard1.1\Mollie.NetStandard1.1.csproj --configfile nuget.config --verbosity Detailed
+dotnet build src\Mollie.NetStandard1.1\Mollie.NetStandard1.1.csproj -c release -p:Version=$framework_version
+
+Write-Host "Build for .Net Standard 2.0"
+dotnet restore src\Mollie.NetStandard2.0\Mollie.NetStandard2.0.csproj --configfile nuget.config --verbosity Detailed
+dotnet build src\Mollie.NetStandard2.0\Mollie.NetStandard2.0.csproj -c release -p:Version=$framework_version
+
+Write-Host "NuGet pack"
+./nuget pack nuget/Package.nuspec -OutputDirectory nuget/output -Properties "BuildConfiguration=release;Version=$nuget_version;PackageVersion=$nuget_version" -Verbosity Detailed
